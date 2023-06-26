680da9211b09e42927b0589d2987e426a3f11038
bensonibeabuchi <ibeabuchibenson@gmail.com>
2023-06-26 08:36:52 +0100

    fix

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,14 +1,10 @@
 "use client";
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import backend from "../backend";
 import Image from "next/image";
-import Link from "next/link";
 import fivestar from "../images/fivestar.png";
 import slide2 from "../images/hero2.png";
 
