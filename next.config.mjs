65ee5beac6b38d0ee8b91bacee7247b842e43cac
bensonibeabuchi <ibeabuchibenson@gmail.com>
2025-12-02 05:54:58 -0800

    added hostname

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
-const supabaseUrl = new URL(process.env.NEXT_PUBLIC_SUPABASE_URL);
-
-
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: supabaseUrl.hostname,
-        pathname: "/**",
-      },
-    ],
-  },
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "https://fpseuxbfvsjpeqxgiffw.supabase.co",
+          port: "",
+          pathname: "/**",
+        },
+      ],
+    },
 };
 
 export default nextConfig;
