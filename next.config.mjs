46831cfb3005ed140bb113bf4ba549c279a6eb78
bensonibeabuchi <ibeabuchibenson@gmail.com>
2025-12-02 06:30:57 -0800

    added conditional statement for my webapp infra

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
       remotePatterns: [
         {
           protocol: "https",
-          hostname: "https://fpseuxbfvsjpeqxgiffw.supabase.co",
+          hostname: "fpseuxbfvsjpeqxgiffw.supabase.co",
           port: "",
           pathname: "/**",
         },
