b771a3be3418cbb05b9be576302c15345583cd18
bensonibeabuchi <ibeabuchibenson@gmail.com>
2025-12-02 05:48:26 -0800

    testing KV 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
+const supabaseUrl = new URL(process.env.NEXT_PUBLIC_SUPABASE_URL);
+
+
 const nextConfig = {
   images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: process.env.NEXT_PUBLIC_SUPABASE_HOST,
-          port: "",
-          pathname: "/**",
-        },
-      ],
-    },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: supabaseUrl.hostname,
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
