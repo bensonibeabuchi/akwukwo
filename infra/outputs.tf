a5ca49e3bddf6a01045460036a49138c387e8e96
bensonibeabuchi <ibeabuchibenson@gmail.com>
2025-12-01 23:05:15 -0800

    fixed test6

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,9 +6,6 @@ output "app_service_url" {
   value = "https://${azurerm_linux_web_app.app.default_hostname}"
 }
 
-output "app_insights_instrumentation_key" {
-  value = azurerm_application_insights.appinsights.instrumentation_key
-}
 
 output "key_vault_name" {
   value = azurerm_key_vault.kv.name
