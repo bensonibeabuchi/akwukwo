1fe8e508f97e17fefa034440acf50d9d1a2ed6d8
bensonibeabuchi <ibeabuchibenson@gmail.com>
2025-12-01 19:18:32 -0800

    added mock for jtest

---
+++ __mocks__/supabaseClient.js
@@ -0,0 +1,13 @@
+export const supabase = {
+  from: jest.fn(() => ({
+    select: jest.fn().mockReturnThis(),
+    insert: jest.fn().mockReturnThis(),
+    update: jest.fn().mockReturnThis(),
+    delete: jest.fn().mockReturnThis(),
+    eq: jest.fn().mockReturnThis(),
+  })),
+  auth: {
+    signInWithPassword: jest.fn(),
+    signOut: jest.fn(),
+  },
+};
