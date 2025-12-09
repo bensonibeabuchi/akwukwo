7503a95fe0cdd58d7369554bff8f0e39acd7a1b3
bensonibeabuchi <ibeabuchibenson@gmail.com>
2025-12-08 18:53:58 -0800

    fixed my test2

--- __mocks__/supabaseClient.js
+++ __mocks__/supabaseClient.js
@@ -1,13 +1,12 @@
 export const supabase = {
   from: jest.fn(() => ({
     select: jest.fn().mockReturnThis(),
-    insert: jest.fn().mockReturnThis(),
-    update: jest.fn().mockReturnThis(),
-    delete: jest.fn().mockReturnThis(),
     eq: jest.fn().mockReturnThis(),
+    order: jest.fn().mockReturnThis(),
+    single: jest.fn().mockResolvedValue({ data: null, error: null }),
   })),
   auth: {
+    getUser: jest.fn(),
     signInWithPassword: jest.fn(),
-    signOut: jest.fn(),
   },
 };
