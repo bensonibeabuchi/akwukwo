e9bcff2626cc148658246331501cf17acfc2c9b1
bensonibeabuchi <ibeabuchibenson@gmail.com>
2025-12-09 18:26:26 -0800

    staging envrionment

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       <div className="flex pt-32">
         <Sidebar/>
         <div className="p-8 flex-1">
-          <h1 className="text-2xl font-bold mb-6">Akwukwo — Courses staging</h1>
+          <h1 className="text-2xl font-bold mb-6">Akwukwo — Courses You should only see this in staging environment</h1>
           {isLoading ? <><div>Loading courses...</div>
           </>: <></>}
           {isError ? <><div>No Courses availble yet</div></> : <><div className="grid gap-6 lg:grid-cols-3 md:grid-cols-2">
